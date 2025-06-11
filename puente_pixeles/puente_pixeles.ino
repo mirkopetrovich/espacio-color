f634a821692138b6a34f167db8d2a40fdec204e9
mirkopetrovich <mirko.petrovich@gmail.com>
2025-06-11 11:08:56 -0400

    patch pd

--- puente_pixeles/puente_pixeles.ino
+++ puente_pixeles/puente_pixeles.ino
@@ -1,28 +1,147 @@
 
 
 #include <Adafruit_NeoPixel.h>
-Adafruit_NeoPixel pixels(27, 6, NEO_RBG + NEO_KHZ800);
+Adafruit_NeoPixel pixels(256, 1, NEO_GRB + NEO_KHZ800);
 int incomingByte = 0; // for incoming serial data
+unsigned char led[66];
 
 void setup() {
-  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  pinMode(LED_BUILTIN, OUTPUT);
+  USBSerial.begin(115200); // opens serial port, sets data rate to 9600 bps
+  pinMode(8, OUTPUT);
   pixels.begin(); 
   pixels.clear();
   pixels.show();
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
 
-  if (incomingByte>=100) pixi(incomingByte-100,255);
-  else pixi(incomingByte,0);
+  if (USBSerial.available() > 0) {
+    USBSerial.readBytes(led,66);
+
+    //if (led[0]>=100) pixi(led[0]-100,255);
+    //else pixi(led[0],0);
+    for (int i=0;i<66;i++) {
+
+    int rojo = 0;
+    int verde = 0;
+    int azul = 0;
+
+    if (led[i]==0) {
+      rojo = 0;
+      verde = 0;
+      azul = 0;
+    }
+
+    else if (led[i]==1) {
+      rojo = 100;
+      verde = 0;
+      azul = 0;
+    }
+
+    else if (led[i]==2) {
+      rojo = 0;
+      verde = 100;
+      azul = 0;
+    }
+
+     else if (led[i]==3) {
+      rojo = 0;
+      verde = 0;
+      azul = 100;
+    }
+
+     else if (led[i]==4) {
+      rojo = 30;
+      verde = 30;
+      azul = 100;
+    }
+
+     else if (led[i]==5) {
+      rojo = 100;
+      verde = 30;
+      azul = 30;
+    }
+
+     else if (led[i]==6) {
+      rojo = 30;
+      verde = 100;
+      azul = 30;
+    }
+
+     else if (led[i]==7) {
+      rojo = 0;
+      verde = 100;
+      azul = 100;
+    }
+
+     else if (led[i]==8) {
+      rojo = 30;
+      verde = 30;
+      azul = 30;
+    }
+
+     else if (led[i]==9) {
+      rojo = 100;
+      verde = 0;
+      azul = 100;
+    }
+
+     else if (led[i]==10) {
+      rojo = 100;
+      verde = 100;
+      azul = 0;
+    }
+
+     else if (led[i]==11) {
+      rojo = 100;
+      verde = 50;
+      azul = 0;
+    }
+
+     else if (led[i]==12) {
+      rojo = 0;
+      verde = 100;
+      azul = 50;
+    }
+
+     else if (led[i]==13) {
+      rojo = 50;
+      verde = 0;
+      azul = 100;
+    }
+
+     else if (led[i]==14) {
+      rojo = 100;
+      verde = 100;
+      azul = 100;
+    }
+
+     else if (led[i]==15) {
+      rojo = 30;
+      verde = 50;
+      azul = 80;
+    }
+
+     else if (led[i]==16) {
+      rojo = 80;
+      verde = 30;
+      azul = 50;
+    }
+
+
+    pixels.setPixelColor((i*3),pixels.Color(rojo,verde,azul));
+    pixels.setPixelColor((i*3)+1,pixels.Color(rojo,verde,azul));
+    pixels.setPixelColor((i*3)+2,pixels.Color(rojo,verde,azul));
+    
+
+    //pixels.setPixelColor(i,pixels.Color(led[i*3],led[(i*3)+1],led[(i*3)+2]));
+    }
+   
   pixels.show();
   }
-
-
 }
+
+
 
 void pixi(int posi,int v) {
 
@@ -30,9 +149,9 @@ void pixi(int posi,int v) {
   //int g = random(255);
   //int b = random(255);
 
-  for (int i=0;i<3;i++) {
   
-        pixels.setPixelColor(i+(posi*3), pixels.Color(0,v,0));
+  
+        pixels.setPixelColor (posi, pixels.Color(0,v,0));
     
-  }
+  
 }
